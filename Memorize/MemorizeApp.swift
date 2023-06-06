ba630895e183c1335e1b1f9367313e7476195e13
fchiarello_username <fchiarello@gmail.com>
2023-06-05 23:02:36 -0300

    Initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Fellipe Ricciardi Chiarello on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
